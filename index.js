c6dc428cac74b12b307614036e9b07eb480c8738
Megan McNeill <mcneim2@gmail.com>
2024-06-22 16:05:16 -0400

    created writetoFile function and started init function by using inquirer to prompt the user with the questions

--- index.js
+++ index.js
@@ -48,10 +48,16 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err)=>
+        err ? console.log(err) : console.log('Successfully created a readme')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
