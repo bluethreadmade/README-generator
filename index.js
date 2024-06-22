39c11e106313439a5399e114a68b0ce83a8d7753
Megan McNeill <mcneim2@gmail.com>
2024-06-21 21:07:22 -0400

    created array of questions

--- index.js
+++ index.js
@@ -3,7 +3,49 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'description',
+        description: 'Describe your project',
+    },
+    {
+        type: 'input',
+        name: 'contents',
+        description: 'Table of Contents',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        description: 'Installation',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        description: 'Usage',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        description: 'License',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        description: 'Contributing',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        description: 'Tests',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        description: 'Questions',
+    },
+   
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
