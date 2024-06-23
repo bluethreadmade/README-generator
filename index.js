73a948bbbc566f7cd014bc438e6a6ea1eb6f5acf
Megan McNeill <mcneim2@gmail.com>
2024-06-22 21:52:34 -0400

    created init function

--- index.js
+++ index.js
@@ -14,49 +14,67 @@ const questions = [
         name: 'contents',
         description: 'Table of Contents',
     },
-    {
-        type: 'input',
-        name: 'installation',
-        description: 'Installation',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        description: 'Usage',
-    },
-    {
-        type: 'input',
-        name: 'license',
-        description: 'License',
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        description: 'Contributing',
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        description: 'Tests',
-    },
-    {
-        type: 'input',
-        name: 'questions',
-        description: 'Questions',
-    },
+    // {
+    //     type: 'input',
+    //     name: 'installation',
+    //     description: 'Installation',
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'usage',
+    //     description: 'Usage',
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'license',
+    //     description: 'License',
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'contributing',
+    //     description: 'Contributing',
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'tests',
+    //     description: 'Tests',
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'questions',
+    //     description: 'Questions',
+    // },
    
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+    
+
+
     fs.writeFile(fileName, data, (err)=>
-        err ? console.log(err) : console.log('Successfully created a readme')
-    );
+            err ? console.log(err) : console.log('Successfully created a readme')
+        );
 }
 
 // TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions);
+function init() {    
+
+    // create a variable for the readme content and what should be in it
+    const generatereadMEContent = ({ description, contents }) =>
+        `$$ ${description}
+        $$ ${contents}`;
+
+    // ask the questions
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            const readMEContent = generatereadMEContent(answers);
+
+            // call the write to file function to create a file called readme1.md and fill it with the readmecontent
+            writeToFile('README1.md', readMEContent);
+        },
+    );
 }
 
 // Function call to initialize app
